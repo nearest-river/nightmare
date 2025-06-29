06096de227b0c3cbe4b9b97f7e91b7be5505e7d8
Nate River <river177169@proton.me>
2025-06-29 14:16:42 +0530

    add assets

---
+++ assets/code1.js
@@ -0,0 +1,13 @@
+
+// debounce.js
+export function debounce(fn, delay) {
+  let timer =null;
+
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
