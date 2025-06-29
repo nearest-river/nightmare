f76e82766d0b3a88bcd68fded7786ee78e20244d
Nate River <river177169@proton.me>
2025-06-29 17:11:12 +0530

    improved functionality of vector

--- src/vector.rs
+++ src/vector.rs
@@ -11,53 +11,59 @@ pub trait VectorExt {
   fn div(self,other: Self)-> Vec<Self::T>;
 
   fn dot(self,other: Self)-> Self::T;
+  fn cosine_similarity(self,other: Self)-> Self::T;
 }
 
 
-impl VectorExt for &[f32] {
-  type T=f32;
+impl VectorExt for &[f64] {
+  type T=f64;
   
-  fn norm(self)-> f32 {
+  fn norm(self)-> f64 {
     self.iter()
     .map(|x| x*x)
-    .sum::<f32>()
+    .sum::<f64>()
     .sqrt()
   }
 
-  fn add(self,other: Self)-> Vec<f32> {
+  fn add(self,other: Self)-> Vec<f64> {
     self.iter()
     .zip(other)
     .map(|(a,b)| a+b)
     .collect::<Vec<_>>()
   }
 
-  fn sub(self,other: Self)-> Vec<f32> {
+  fn sub(self,other: Self)-> Vec<f64> {
     self.iter()
     .zip(other)
     .map(|(a,b)| a-b)
     .collect::<Vec<_>>()
   }
 
-  fn mul(self,other: Self)-> Vec<f32> {
+  fn mul(self,other: Self)-> Vec<f64> {
     self.iter()
     .zip(other)
     .map(|(a,b)| a*b)
     .collect::<Vec<_>>()
   }
 
-  fn div(self,other: Self)-> Vec<f32> {
+  fn div(self,other: Self)-> Vec<f64> {
     self.iter()
     .zip(other)
     .map(|(a,b)| a/b)
     .collect::<Vec<_>>()
   }
 
-  fn dot(self,other: Self)-> f32 {
+  fn dot(self,other: Self)-> f64 {
     self.iter()
     .zip(other)
     .map(|(a,b)| a*b)
-    .sum::<f32>()
+    .sum::<f64>()
     .sqrt()
+  }
+
+  #[inline]
+  fn cosine_similarity(self,other: Self)-> Self::T {
+    self.dot(other)/(self.norm()*other.norm())
   }
 }
 
